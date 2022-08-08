0f64d5bbb066e74a3dea6dee50cecf92fb948f35
Hole <376220459@qq.com>
2022-08-08 15:09:30 +0800

    完成验证登录状态逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: Hole 376220459@qq.com
  * @Date: 2022-08-01 20:12:02
  * @LastEditors: Hole 376220459@qq.com
- * @LastEditTime: 2022-08-07 23:00:12
+ * @LastEditTime: 2022-08-08 15:02:15
  * @FilePath: \campus-grocery\src\router\index.js
  * @Description: 路由配置
  */
@@ -12,6 +12,8 @@ import ErrorPage from '../views/ErrorPage.vue'
 import HomePage from '../views/HomePage.vue'
 import VueCookies from 'vue-cookies'
 import { checkAuthToken } from '@/apis/userAccount'
+import resHandle from '@/utils/resHandle'
+import { Message } from 'element-ui'
 
 Vue.use(VueRouter)
 
@@ -38,15 +40,38 @@ const router = new VueRouter({
   routes,
 })
 
+// 封装跳转登录页面函数
+function toLoginPage() {
+  // 跳转前先删除旧的登录token
+  VueCookies.remove('auth_token')
+
+  // 延迟跳转,给用户足够的时间看提示信息
+  setTimeout(() => {
+    const loginURL = `${process.env.VUE_APP_LOGIN}?redirect=${window.location.href}`
+    window.location.href = loginURL
+  }, 2000)
+}
+
+// 因为本系统所有页面均需要登录才可以查看，所以设置全局路由的登录状态验证
 router.beforeEach(async (to, from, next) => {
   const authToken = VueCookies.get('auth_token')
   if (!authToken) {
-    window.location = `${process.env.VUE_APP_LOGIN}?redirect=${window.location.href}`
+    Message.warning('您还没有登录，请先登录')
+    toLoginPage()
   } else {
     const res = await checkAuthToken()
-    alert(JSON.stringify(res))
+    resHandle(res, {
+      successHandle: () => {
+        next()
+      },
+      warningHandle: () => {
+        toLoginPage()
+      },
+      errorHandle: () => {
+        toLoginPage()
+      },
+    })
   }
-  next()
 })
 
 export default router
